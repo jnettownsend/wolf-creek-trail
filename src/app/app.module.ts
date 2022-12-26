0fa8c4f96d63cf1667077eff7824a9bdda6bebb9
Jeannette Townsend <111323478+jnettownsend@users.noreply.github.com>
2022-12-26 06:56:50 -0600

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MapsComponent } from './maps/maps.component';
+import { EventsComponent } from './events/events.component';
+import { LogComponent } from './log/log.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MapsComponent,
+    EventsComponent,
+    LogComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,

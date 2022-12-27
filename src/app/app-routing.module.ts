5a48f8170260a21f7160e8816c77d9782a23187f
Jeannette Townsend <111323478+jnettownsend@users.noreply.github.com>
2022-12-27 15:45:48 -0600

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EventsComponent } from './events/events.component';
+import { HomeComponent } from './home/home.component';
+import { LogComponent } from './log/log.component';
+import { MapsComponent } from './maps/maps.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "home", component: HomeComponent },
+  { path: "maps", component: MapsComponent},
+  { path: "log", component: LogComponent },
+  { path: "events", component: EventsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
